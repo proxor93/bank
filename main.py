a450ac82278749ad81b306391a11755c88a5c02c
Andrey Shtager <shulc1989@yandex.ru>
2024-01-21 20:05:48 +0300

    added variables README

---
+++ main.py
@@ -0,0 +1,13 @@
+exchange_rate = {
+    "dollar": 88.59,
+    "euro": 96.58,
+    "yuan": 12.27,
+}
+
+credits = {
+    "mortgage": 2_432_500,
+    "consumer": 650_000,
+    "auto": 1_800_000
+}
+
+
